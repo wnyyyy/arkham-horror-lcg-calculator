ceab7a196baf5973816ca8ff18ccbb663cbf3a6f
wnyyyy <viniberizzi@gmail.com>
2023-08-28 01:23:44 -0300

    Add color palette

---
+++ lib/presentation/themes/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primaryColor = const Color(0xff1C3F60);
+  static const secondaryColor = const Color(0xffAFC1D0);
+  static const scaffoldColor = const Color(0xff0B1320);
+}

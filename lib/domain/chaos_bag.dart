87403f76e546c6046650d47e3dd1862c9a853c8e
wnyyyy <viniberizzi@gmail.com>
2023-09-01 12:26:51 -0300

    Add getter for allowed tokens

--- lib/domain/chaos_bag.dart
+++ lib/domain/chaos_bag.dart
@@ -2,7 +2,6 @@ import 'package:arkham_horror_lcg_calculator/domain/token.dart';
 
 class ChaosBag {
   static late List<Token> tokens;
-
   static List<SignToken> get signs =>
       tokens.where((token) => token is SignToken).cast<SignToken>().toList();
   static List<NumberToken> get numbers => tokens
@@ -10,4 +9,18 @@ class ChaosBag {
       .cast<NumberToken>()
       .toList();
   static List<Token> get unique => tokens.toSet().toList();
+  static Set<Token> get allowedTokens => <Token>{
+        NumberToken(1),
+        NumberToken(0),
+        NumberToken(-1),
+        NumberToken(-2),
+        NumberToken(-3),
+        NumberToken(-4),
+        SignToken(SignType.skull),
+        SignToken(SignType.cultist),
+        SignToken(SignType.tablet),
+        SignToken(SignType.elderThing),
+        SignToken(SignType.autoFail),
+        SignToken(SignType.elderSign),
+      };
 }
